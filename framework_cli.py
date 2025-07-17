bdee3f5e9657102f281125dfa14c8c0f2d8b09af
Sushant Kalamani <sushantkalamani@gmail.com>
2025-07-17 12:45:15 +0530

    completed 3 points

--- framework_cli.py
+++ framework_cli.py
@@ -2,7 +2,7 @@
 # python framework_cli.py ingest:sample_configs_and_templates/ingestion/ingestion_template.json add-lineage:sample_configs_and_templates/lineage/dataset_lineage_template.json 
 import argparse # 
 import logging
-from core.controllers import ingestion_controller, lineage_controller
+from core.controllers import ingestion_controller
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ def main():
     parser.add_argument(
         "operations",
         nargs='+',
-        help="A list of operations to perform, in the format 'operation:config_path'. "
+        help="A list of operations to perform, in the format 'operation:folder_path'. "
              "e.g., ingest:configs/ingestion.json"
     )
 
@@ -22,20 +22,20 @@ def main():
     for op_config in args.operations:
         try:
             # Split the argument into the operation and its config path
-            operation, config_path = op_config.split(':', 1)
+            operation, folder_path = op_config.split(':', 1)
             
-            logger.info(f"Executing operation '{operation}' with config '{config_path}'")
+            logger.info(f"Executing operation '{operation}' with config '{folder_path}'")
 
             if operation.lower() == "ingest":
-                ingestion_controller.run_ingestion(config_path)
-            elif operation.lower() == "add-lineage":
-                lineage_controller.run_add_lineage(config_path)
+                ingestion_controller.run_ingestion(folder_path)
+            # elif operation.lower() == "add-lineage":
+            #     lineage_controller.run_add_lineage(folder_path)
             # Add other operations like "enrich" here in the future
             else:
                 logger.error(f"Unknown operation: {operation}")
 
         except ValueError:
-            logger.error(f"Invalid operation format: '{op_config}'. Expected 'operation:config_path'.")
+            logger.error(f"Invalid operation format: '{op_config}'. Expected 'operation:folder_path'.")
         except Exception as e:
             logger.error(f"A critical error occurred during operation '{op_config}': {e}", exc_info=True)
 
